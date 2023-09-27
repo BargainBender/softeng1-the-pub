67006838ee8c15bd030354dd68836a0f1f6f4b1c
Jace L. Gonzales <jacegonzales25@gmail.com>
2023-09-27 12:54:40 +0800

    Initial Signup page

---
+++ client/app/(auth)/components/user-signup-form.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React,{ useState } from "react";
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Icons } from "@/components/ui/icons";
+
+interface LoadingState {
+    isLoading: boolean;
+}
+
+const UserSignUpForm: React.FC = () => {
+    const [isLoading, setIsLoading] = React.useState<boolean>(false)
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default UserSignUpForm;
