e6c94ca216ac3adfbc3b89446944d58888cac716
Jace L. Gonzales <jacegonzales25@gmail.com>
2023-10-06 16:43:33 +0800

    Sign-up Page Progress
    Modified Input component for the pub, added button Variant. Configured
    sign-up page

--- frontend/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ frontend/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,85 +1,79 @@
-
-
 import { Metadata } from "next";
 
 import Image from "next/image";
 import Link from "next/link";
 
-
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import  UserSignUpForm  from "@/app/(auth)/components/user-signup-form";
-
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import UserSignUpForm from "@/app/(auth)/components/user-signup-form";
 
 export const metadata: Metadata = {
-    title: "Authentication",
-    description: "Authentication forms built using the components.",
-}
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+};
 
 export default function AuthenticationPage() {
-    return (
-        <>
-            {/* <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-                <Link
-                    href="/sign-up"
-                    className={cn(
-                        buttonVariants({ variant: "ghost" }),
-                        "absolute right-4 top-4 md:right-8 md:top-8"
-                    )}
-                >
-                    Login
-                </Link>
-            </div> */}
-            <div className="relative hidden h-full flex-col p-10 text-black dark:border-r lg:flex flex-1">
-                <div className="absolute inset-0 flex items-center justify-center">
-                    <div className="relative z-20 flex items-center text-lg font-medium">
-                        <Image 
-<<<<<<< HEAD:client/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
-                        src={"/public/images/thepublogo.png"}
-                        alt="The Pub Logo"
-                        />
-                        The Pub Logo
-=======
-                            src="/thepublogo.png"
-                            alt="The Pub Logo"
-                            width={200}
-                            height={200}
-                        />
->>>>>>> 667baa36911111ec7239fb135ff9145668611774:frontend/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
-                    </div>
-                </div>
-                <div className="relative z-20 mt-auto">
-                    <blockquote className="space-y-2">
-                        <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-                            &ldquo;Come for the Stories, Stay for the Community!&rdquo;
-                        </h2>
-                    </blockquote>
-                </div>
+  return (
+    <>
+      <div className="grid grid-cols-[1.05fr_1fr] w-full h-screen">
+        <div className="relative hidden h-full flex-col p-10 text-black dark:border-r lg:w-3/15 lg:flex flex-1">
+          <div className="absolute inset-0 flex items-center justify-end">
+            <div className="relative z-20 flex items-center text-lg font-medium">
+              <Image
+                src="/thepublogo.png"
+                alt="The Pub Logo"
+                width={200}
+                height={200}
+              />
+              <blockquote className="space-y-2">
+                <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                  &ldquo;Come for the Stories,
+                  <br />
+                  Stay for the Community!&rdquo;
+                </h2>
+              </blockquote>
             </div>
-            <div className="lg:p-8 flex-1">
-                <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-                    <div className="flex flex-col space-y-2 text-center">
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Create your Account</CardTitle>
-                                <CardDescription>In just a few steps!</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <UserSignUpForm />
-                            </CardContent>
-                        </Card>
-                        
-                    </div>
-                </div>
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                &ldquo;Come for the Stories, Stay for the Community!&rdquo;
+              </h2>
+            </blockquote>
+          </div>
+        </div>
+        <div className="lg:p-8 my-auto">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+            <div className="lg:p-8 my-auto">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Create your Account</CardTitle>
+                  <CardDescription>In just a few steps!</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <UserSignUpForm />
+                </CardContent>
+              </Card>
             </div>
-        </>
-    )
+          </div>
+        </div>
+             
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </>
+  );
 }
--- frontend/app/(auth)/components/user-signup-form.tsx
+++ frontend/app/(auth)/components/user-signup-form.tsx
@@ -8,29 +8,81 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Icons } from "@/components/ui/icons";
 
-interface LoadingState {
-  isLoading: boolean;
-}
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-const UserSignUpForm: React.FC = () => {
+interface UserSignUpFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function UserSignUpForm({ className, ...props }: UserSignUpFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
   return (
     <>
-      <div className="grid w-full gap-4">
-        <div className="flex flex-col space-y-1.5 items-start">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" placeholder="Email"></Input>
-            <Label htmlFor="firstname">First Name</Label>
-            <Input id="firstname" placeholder="First Name"></Input>
-            <Label htmlFor="lastname">Last Name</Label>
-            <Input id="lastname" placeholder="Last Name"></Input>
+      <form onSubmit={onSubmit}>
+        <div className="grid w-full gap-4">
+          <div className="grid gap-4">
+            <Label htmlFor="email" className="sr-only">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            ></Input>
+            <Label htmlFor="firstname" className="sr-only padding-1">
+              First Name
+            </Label>
+            <Input
+              id="firstname"
+              placeholder="First Name"
+              type="text"
+              autoCapitalize="none"
+              autoComplete="firstname"
+              autoCorrect="off"
+              disabled={isLoading}
+            ></Input>
+            <Label htmlFor="lastname" className="sr-only padding-1">
+              Last Name
+            </Label>
+            <Input
+              id="lastname"
+              placeholder="Last Name"
+              type="text"
+              autoCapitalize="none"
+              autoComplete="lastname"
+              autoCorrect="off"
+              disabled={isLoading}
+            ></Input>
+          </div>
+          <div className="flex flex-col space-y-1.5">
+            <Button variant={"pub"} className="pub" disabled={isLoading}>
+              {isLoading && (
+                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Continue
+            </Button>
+          </div>
         </div>
-        <div className="flex flex-col space-y-1.5">
-                <Button variant={"pub"} className="pub">Continue</Button>
-        </div>
-      </div>
+      </form>
     </>
   );
-};
+}
 
 export default UserSignUpForm;
