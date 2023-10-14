c4636ba3e87d9be709c396a701f60a8cb4b704fb
Jace L. Gonzales <jacegonzales25@gmail.com>
2023-10-14 15:35:27 +0800

    Finished Stepping Functionality - Require back-end functionality
    Deleted components that renders the fragments, made it into one form
    instead. Using use case, the page can render different steps. Will need
    to add smoothing animation and async function for loading stage for User
    Experience. TODO: Add form and submitting functionality.

--- frontend/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ frontend/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,81 @@
-import EmailName from "./components/email-name";
-import PasswordConfirm from "./components/password-input";
-import { Checkbox } from "@/components/ui/checkbox";
+"use client";
 
-export default function AuthenticationPage() {
+import Image from "next/image";
+
+
+import { useState } from "react";
+
+import UserSignUpForm from "@/app/(auth)/components/user-signup-form";
+
+export interface UserData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  password: string;
+}
+
+export default function SignUpPage() {
+  const [userData, setUserData] = useState<UserData>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  const handleFormSubmit = (data: UserData) => {
+    setUserData({ ...userData, ...data });
+  };
+
   return (
     <>
-      <PasswordConfirm />
+      <div className="grid grid-cols-[1.05fr_1fr] w-full h-screen">
+        <div className="relative hidden h-full flex-col p-10 text-black dark:border-r lg:w-3/15 lg:flex flex-1">
+          <div className="absolute inset-0 flex items-center justify-end">
+            <div className="relative z-20 flex items-center text-lg font-medium">
+              <Image
+                src="/thepublogo.png"
+                alt="The Pub Logo"
+                width={400}
+                height={200}
+              />
+            </div>
+          </div>
+          <div className="flex items-center justify-end relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <h1 className="scroll-m-20 pb-2 text-4xl font-semibold tracking-wide transition-colors first:mt-0">
+                Come for the Stories, {<br />}Stay for the Community!
+              </h1>
+            </blockquote>
+          </div>
+        </div>
+        <div className="grid grid-cols-[1.05fr_1fr] w-full h-screen">
+          <div className="lg:p-8 my-auto">
+            <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+              <div className="flex flex-col space-y-2 text-center">
+                <div className="lg:p-8 my-auto">
+                  <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                    <div className="flex flex-col space-y-2 text-center"> 
+                         <UserSignUpForm />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- frontend/app/(auth)/components/user-signup-form.tsx
+++ frontend/app/(auth)/components/user-signup-form.tsx
@@ -1,17 +1,40 @@
 "use client";
 
 import React, { useState } from "react";
+import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Icons } from "@/components/ui/icons";
-
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 interface UserSignUpFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+interface SignUpFormState {
+  step: number;
+}
+
 export function UserSignUpForm({ className, ...props }: UserSignUpFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [state, setState] = useState<SignUpFormState>({
+    step: 0,
+  });
+
+  const handleContinue = () => {
+    setState((prevState) => ({
+      ...prevState,
+      step: prevState.step + 1,
+    }));
+  };
+
   async function onSubmit(event: React.SyntheticEvent) {
     event.preventDefault();
     setIsLoading(true);
@@ -20,60 +43,271 @@ export function UserSignUpForm({ className, ...props }: UserSignUpFormProps) {
       setIsLoading(false);
     }, 3000);
   }
-  return (
-    <>
-      <form onSubmit={onSubmit}>
-        <div className="grid w-full gap-4">
-          <div className="grid gap-4">
-            <Label htmlFor="email" className="sr-only">
-              Email
-            </Label>
-            <Input
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-            ></Input>
-            <Label htmlFor="firstname" className="sr-only padding-1">
-              First Name
-            </Label>
-            <Input
-              id="firstname"
-              placeholder="First Name"
-              type="text"
-              autoCapitalize="none"
-              autoComplete="firstname"
-              autoCorrect="off"
-              disabled={isLoading}
-            ></Input>
-            <Label htmlFor="lastname" className="sr-only padding-1">
-              Last Name
-            </Label>
-            <Input
-              id="lastname"
-              placeholder="Last Name"
-              type="text"
-              autoCapitalize="none"
-              autoComplete="lastname"
-              autoCorrect="off"
-              disabled={isLoading}
-            ></Input>
-          </div>
-          <div className="flex flex-col space-y-1.5">
-            <Button variant={"pub"} className="pub" disabled={isLoading}>
-              {isLoading && (
-                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-              )}
-              Continue
-            </Button>
-          </div>
-        </div>
-      </form>
-    </>
-  );
+
+  const renderStep = () => {
+    switch (state.step) {
+      case 0:
+        return (
+          <Card>
+            <CardHeader>
+              <CardTitle>Create your Account</CardTitle>
+              <CardDescription>In just a few steps!</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid w-full gap-4">
+                <div className="grid gap-4">
+                  <Label htmlFor="email" className="sr-only">
+                    Email
+                  </Label>
+                  <Input
+                    id="email"
+                    placeholder="name@example.com"
+                    type="email"
+                    autoCapitalize="none"
+                    autoComplete="email"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                  />
+                  <Label htmlFor="firstname" className="sr-only padding-1">
+                    First Name
+                  </Label>
+                  <Input
+                    id="firstname"
+                    placeholder="First Name"
+                    type="text"
+                    autoCapitalize="none"
+                    autoComplete="firstname"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                  />
+                  <Label htmlFor="lastname" className="sr-only padding-1">
+                    Last Name
+                  </Label>
+                  <Input
+                    id="lastname"
+                    placeholder="Last Name"
+                    type="text"
+                    autoCapitalize="none"
+                    autoComplete="lastname"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                  />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Button variant={"pub"} className="pub" disabled={isLoading}>
+                    {isLoading && (
+                      <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                    )}
+                    Continue
+                  </Button>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        );
+      case 1:
+        return (
+          <Card>
+            <CardHeader>
+              <CardTitle className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                Let{"'"}s secure your <br /> account
+              </CardTitle>
+              <CardDescription></CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid w-full gap-4">
+                <div className="grid gap-4">
+                  <Label htmlFor="username" className="sr-only">
+                    Username
+                  </Label>
+                  <Input
+                    id="username"
+                    placeholder="Username"
+                    type="text"
+                    autoCapitalize="none"
+                    autoComplete="email"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                  ></Input>
+                  <Label htmlFor="firstname" className="sr-only padding-1">
+                    Password
+                  </Label>
+                  <Input
+                    id="password"
+                    placeholder="Password"
+                    type="password"
+                    autoCapitalize="none"
+                    autoComplete="firstname"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                  ></Input>
+                  <Label htmlFor="lastname" className="sr-only padding-1">
+                    Confirm Password
+                  </Label>
+                  <Input
+                    id="confirm-password"
+                    placeholder="Confirm Password"
+                    type="password"
+                    autoCapitalize="none"
+                    autoComplete="lastname"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                  ></Input>
+                </div>
+
+                <div className="flex items-center space-x-2">
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    Accept terms and conditions
+                  </label>
+                </div>
+                <div className="flex items-start space-x-2">
+                  <p className="text-sm text-muted-foreground">
+                    You agree to our{" "}
+                    {
+                      <Link
+                        href={"/terms-of-service"}
+                        className="underline underline-offset-3"
+                      >
+                        Terms and Conditions
+                      </Link>
+                    }{" "}
+                    and{" "}
+                    {
+                      <Link
+                        href={"/privacy-policy"}
+                        className="underline underline-offset-4"
+                      >
+                        Privacy Policy
+                      </Link>
+                    }
+                    .
+                  </p>
+                </div>
+
+                <div className="flex flex-col space-y-1.5">
+                  <Button variant={"pub"} className="pub" disabled={isLoading}>
+                    {isLoading && (
+                      <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                    )}
+                    Register
+                  </Button>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        );
+      case 2:
+        return (
+          <Card>
+            <CardHeader>
+              <CardTitle className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                Successfully Registered!
+              </CardTitle>
+              <CardDescription></CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid w-full gap-4">
+                <div className="grid gap-4">
+                  <Label htmlFor="email" className="sr-only">
+                    Email
+                  </Label>
+                  <Input
+                    id="email"
+                    placeholder={"name@example.com"} //UserData.email
+                    type="email"
+                    autoCapitalize="none"
+                    autoComplete="email"
+                    autoCorrect="off"
+                    disabled={true}
+                  ></Input>
+                  <Label htmlFor="username" className="sr-only padding-1">
+                    Username
+                  </Label>
+                  <Input
+                    id="username"
+                    placeholder={"Username"} //UserData.username
+                    type="text"
+                    autoCapitalize="none"
+                    autoComplete="username"
+                    autoCorrect="off"
+                    disabled={true}
+                  ></Input>
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Button variant={"pub"} className="pub" disabled={false}>
+                    Login
+                  </Button>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        );
+      default:
+        // Render the last step of the form here.
+        break;
+    }
+  };
+
+  return <form onSubmit={onSubmit}>{renderStep()}</form>;
+
+  // return (
+  //   <>
+  //     <form onSubmit={onSubmit}>
+  //       <div className="grid w-full gap-4">
+  //         <div className="grid gap-4">
+  //           <Label htmlFor="email" className="sr-only">
+  //             Email
+  //           </Label>
+  //           <Input
+  //             id="email"
+  //             placeholder="name@example.com"
+  //             type="email"
+  //             autoCapitalize="none"
+  //             autoComplete="email"
+  //             autoCorrect="off"
+  //             disabled={isLoading}
+  //           ></Input>
+  //           <Label htmlFor="firstname" className="sr-only padding-1">
+  //             First Name
+  //           </Label>
+  //           <Input
+  //             id="firstname"
+  //             placeholder="First Name"
+  //             type="text"
+  //             autoCapitalize="none"
+  //             autoComplete="firstname"
+  //             autoCorrect="off"
+  //             disabled={isLoading}
+  //           ></Input>
+  //           <Label htmlFor="lastname" className="sr-only padding-1">
+  //             Last Name
+  //           </Label>
+  //           <Input
+  //             id="lastname"
+  //             placeholder="Last Name"
+  //             type="text"
+  //             autoCapitalize="none"
+  //             autoComplete="lastname"
+  //             autoCorrect="off"
+  //             disabled={isLoading}
+  //           ></Input>
+  //         </div>
+  //         <div className="flex flex-col space-y-1.5">
+  //           <Button variant={"pub"} className="pub" disabled={isLoading}>
+  //             {isLoading && (
+  //               <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+  //             )}
+  //             Continue
+  //           </Button>
+  //         </div>
+  //       </div>
+  //     </form>
+  //   </>
+  // );
 }
 
 export default UserSignUpForm;
