9d573514c4dc4041938e52652106d1105b85b3b6
Sebastian Martinez <sgmartinezlp@gmail.com>
2023-10-29 18:18:46 +0800

    Thread serial improved, urls rearranged

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,7 +6,7 @@ from core.views import UserProfileDetailUpdateAPIView, UserListAPIView
 urlpatterns = [
     
     path('articles/', views.ArticleListCreateView.as_view()),
-    path('<str:username>/', UserProfileDetailUpdateAPIView.as_view()),
-    path('users/', UserListAPIView.as_view()), # TODO: Remove in the future, only for development
     path('threads/', views.ThreadListCreateAPIView.as_view()),
+    path('users/', UserListAPIView.as_view()), # TODO: Remove in the future, only for development
+    path('<str:username>/', UserProfileDetailUpdateAPIView.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -10,6 +10,9 @@ from .serializers import ListArticleSerializer, CreateArticleSerializer, ThreadS
 from .permissions import IsStaffEditorPermission
 from .authentication import TokenAuthentication
 
+from core.models import UserProfile
+from core.serializers import ArticleUserProfileSerializer
+
 # Create your views here.
 
 class ArticleListCreateView(generics.ListCreateAPIView):
@@ -28,13 +31,44 @@ class ArticleListCreateView(generics.ListCreateAPIView):
         elif self.request.method == "POST":
             return CreateArticleSerializer
 
-
 class ThreadListCreateAPIView(generics.ListCreateAPIView):
-    serializer_class = ThreadSerializer
-    queryset = Thread.objects.all()
+    # queryset = Thread.objects.all()
     
-    def get_queryset(self):
-      return Thread.objects.filter(parent__isnull=True)
+    # def get_queryset(self):
+    #   return Thread.objects.filter(parent__isnull=True)
+    
+    def get_serializer_class(self):
+        return ThreadSerializer
+    
+    # TODO: Needs Optimization
+    def get_all_authors(self, thread):
+        authors = [thread.author.user.id]
+        for child in thread.children.all():
+            authors.extend(self.get_all_authors(child))
+        return authors
+
+    # TODO: Needs Optimization
+    def get(self, request):
+        threads = Thread.objects.filter(parent__isnull=True)
+        authors = set()
+        for thread in threads:
+            authors.update(self.get_all_authors(thread))
+        authors = UserProfile.objects.filter(user__id__in=authors)
+
+        author_serializer = ArticleUserProfileSerializer(authors, many=True)
+        author_data = {
+            author['id']: author for author in author_serializer.data
+        }
+
+        thread_serializer = ThreadSerializer(threads, many=True)
+        thread_data = thread_serializer.data
+        for author in author_data:
+            del author_data[author]['id']
+
+        return Response({
+            "authors": author_data,
+            "threads": thread_data
+        })
 
 
 @api_view(['GET'])
