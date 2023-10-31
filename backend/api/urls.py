8974098f16159e98fcee5c4d9176bc05dc1ccfdd
Raymond Postrero <raymondpostrero1@gmail.com>
2023-11-01 00:16:10 +0800

    updated url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,9 +6,9 @@ from core.views import UserProfileDetailUpdateAPIView, UserListAPIView, FollowAP
 urlpatterns = [
     
     path('articles/', views.ArticleListCreateView.as_view()),
-    path('<str:username>/articles/', views.UserArticlesView.as_view(), name='user_articles'),
     path('threads/', views.ThreadListCreateAPIView.as_view()),
     path('users/', UserListAPIView.as_view()), # TODO: Remove in the future, only for development
     path('follow/<str:username>/', FollowAPIView.as_view()),
     path('<str:username>/', UserProfileDetailUpdateAPIView.as_view()),
+    path('<str:username>/articles/', views.UserArticlesView.as_view(), name='user_articles'),
 ]
