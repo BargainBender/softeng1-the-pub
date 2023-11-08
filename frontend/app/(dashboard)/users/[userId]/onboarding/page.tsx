acbcf93db2803ab86404808b389258a9f43ff50b
jacegonzales25 <jacegonzales25@gmail.com>
2023-11-08 12:43:37 +0800

    feat: start with onboarding after sign-up

---
+++ frontend/app/(dashboard)/users/[userId]/onboarding/page.tsx
@@ -0,0 +1,32 @@
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { AspectRatio } from "@/components/ui/aspect-ratio"
+
+
+export default function OnboardingPage(){
+    return (
+        <>
+        <div className="w-full h-full">
+            <div className="flex flex-row">
+            <div className="flex-1">
+                Image
+            </div>
+            <div className="flex-1">
+                <p>
+                    Welcome to the Pub
+                </p>
+            </div>
+
+
+            </div>
+
+            <div>
+                
+            </div>
+           
+        </div>
+       
+        
+        </>
+    )
+}
