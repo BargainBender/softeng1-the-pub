8a1e5345486f799e1d9f32680e8b38b9f33033fb
jacegonzales25 <jacegonzales25@gmail.com>
2023-11-18 22:12:18 +0800

    feat: add card for username and email change

--- frontend/app/(dashboard)/users/[userId]/settings/components/account-form.tsx
+++ frontend/app/(dashboard)/users/[userId]/settings/components/account-form.tsx
@@ -1,14 +1,33 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { CalendarIcon, CheckIcon, ChevronUp } from "lucide-react";
-import { format } from "date-fns";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
 import {
   Form,
   FormControl,
@@ -94,40 +113,139 @@ export function AccountForm() {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="raymondpostrero" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your username. It can be your real name or a pseudonym.
-                This will be username for all The Pub interactions
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is the name that will be displayed on your profile and in
-                emails.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <div className="flex items-center justify-center">
+          <Tabs defaultValue="account" className="w-[500px]">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="account">Account</TabsTrigger>
+              <TabsTrigger value="password">Password</TabsTrigger>
+            </TabsList>
+            <TabsContent value="account">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Account</CardTitle>
+                  <CardDescription>
+                    Make changes to your account here. Click save when you{"'"}
+                    re done.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div className="space-y-1">
+                    <FormField
+                      control={form.control}
+                      name="username"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Username</FormLabel>
+                          <FormControl>
+                            <Input placeholder="raymondpostrero" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="space-y-1">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input placeholder="" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Button>Save changes</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+            <TabsContent value="password">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Password</CardTitle>
+                  <CardDescription>
+                    Change your password here. After saving, you{"'"}ll be
+                    logged out.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div className="space-y-1">
+                    <Label htmlFor="current">Current password</Label>
+                    <Input id="current" type="password" />
+                  </div>
+                  <div className="space-y-1">
+                    <Label htmlFor="new">New password</Label>
+                    <Input id="new" type="password" />
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Button>Save password</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+          </Tabs>
+        </div>
+        <Dialog>
+          <DialogTrigger className="w-[650px] bg-pub rounded">
+            Edit Profile
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when {"you're"}{" "}
+                done.
+              </DialogDescription>
+            </DialogHeader>
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="raymondpostrero" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your username. It can be your real name or a
+                    pseudonym. This will be username for all The Pub
+                    interactions
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is the name that will be displayed on your profile and
+                    in emails.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <DialogClose>
+                <Button>Confirm</Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
         <div>
           <h3 className="mb-4 text-lg font-medium">Privacy</h3>
           <div className="space-y-4">
@@ -158,10 +276,12 @@ export function AccountForm() {
           name="passwordForm.newPassword"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Pronouns</FormLabel>
+              <FormLabel>New password</FormLabel>
               <Input type="password" {...field} />
               <FormDescription>
-                This will be your pronouns for the site.
+                Password must have at least one lowercase letter, one uppercase
+                letter, one number, one special character, and be at least 8
+                characters long.{" "}
               </FormDescription>
               <FormMessage />
             </FormItem>
@@ -172,14 +292,13 @@ export function AccountForm() {
           name="passwordForm.confirm"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Pronouns</FormLabel>
+              <FormLabel>Confirm new password</FormLabel>
               <Input type="password" {...field} />
               <FormDescription></FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-
         <Button type="submit">Update account</Button>
       </form>
     </Form>
