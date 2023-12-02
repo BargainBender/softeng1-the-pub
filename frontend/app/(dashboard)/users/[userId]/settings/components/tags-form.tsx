9f55e3d6eadeafe1f6cca90cc32fbc18719d377f
jacegonzales25 <jacegonzales25@gmail.com>
2023-12-02 19:18:37 +0800

    fix: refactor tags form, remove unnecessary notifications form

--- frontend/app/(dashboard)/users/[userId]/settings/components/tags-form.tsx
+++ frontend/app/(dashboard)/users/[userId]/settings/components/tags-form.tsx
@@ -1,11 +1,13 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
+import { useState, useEffect } from "react";
 
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import {
   Form,
   FormControl,
@@ -14,97 +16,87 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { Switch } from "@/components/ui/switch"
-import { toast } from "@/components/ui/use-toast"
+} from "@/components/ui/form";
+import { toast } from "@/components/ui/use-toast";
 
 const tagsFormSchema = z.object({
-  type: z.enum(["all", "mentions", "none"], {
-    required_error: "You need to select a notification type.",
-  }),
   tags: z.array(z.string()),
-})
+});
 
-type TagsFormValues = z.infer<typeof tagsFormSchema>
+type TagsFormValues = z.infer<typeof tagsFormSchema>;
 
 // This can come from your database or API.
-const defaultValues: Partial<TagsFormValues> = {
-  // academics: false,
-  // travel: false,
-  // entertainment: true,
-  // technology: true,
-  // sports: true,
+const defaultValues: Partial<TagsFormValues> = {};
+
+interface TagsFormProps {
+  allTags: string[];
+  onChosenTagsChange: (chosenTags: string[]) => void; // Callback function
 }
 
-export function TagsForm() {
-  const form = useForm<TagsFormValues>({
-    resolver: zodResolver(tagsFormSchema),
-    defaultValues,
-  })
+export function TagsForm({
+  allTags,
+  onChosenTagsChange,
+}: TagsFormProps) {
+  const [chosenTags, setChosenTags] = useState<string[]>([]);
+  const [unchosenTags, setUnchosenTags] = useState<string[]>(allTags);
 
-  function onSubmit(data: TagsFormValues) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    })
-  }
+  const handleTagClick = (tag: string) => {
+    if (chosenTags.includes(tag)) {
+      setChosenTags((prevTags) =>
+        prevTags.filter((prevTag) => prevTag !== tag)
+      );
+      setUnchosenTags((prevTags) => [...prevTags, tag]);
+    } else {
+      setChosenTags((prevTags) => [...prevTags, tag]);
+      setUnchosenTags((prevTags) =>
+        prevTags.filter((prevTag) => prevTag !== tag)
+      );
+    }
+  };
+
+  // Notify the parent component when chosenTags change
+  useEffect(() => {
+    onChosenTagsChange(chosenTags);
+  }, [chosenTags, onChosenTagsChange]);
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="type"
-          render={({ field }) => (
-            <FormItem className="space-y-3">
-              <FormLabel>Notify me about...</FormLabel>
-              <FormControl>
-                <RadioGroup
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  className="flex flex-col space-y-1"
-                >
-                  <FormItem className="flex items-center space-x-3 space-y-0">
-                    <FormControl>
-                      <RadioGroupItem value="all" />
-                    </FormControl>
-                    <FormLabel className="font-normal">
-                      All new messages
-                    </FormLabel>
-                  </FormItem>
-                  <FormItem className="flex items-center space-x-3 space-y-0">
-                    <FormControl>
-                      <RadioGroupItem value="mentions" />
-                    </FormControl>
-                    <FormLabel className="font-normal">
-                      Direct messages and mentions
-                    </FormLabel>
-                  </FormItem>
-                  <FormItem className="flex items-center space-x-3 space-y-0">
-                    <FormControl>
-                      <RadioGroupItem value="none" />
-                    </FormControl>
-                    <FormLabel className="font-normal">Nothing</FormLabel>
-                  </FormItem>
-                </RadioGroup>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div>
-          <h3 className="mb-4 text-lg font-medium">Tags Suggestions</h3>
-          <div className="space-y-4">
-        
-          </div>
+    <div className="flex flex-col items-center">
+      <div className="flex justify-center space-x-2">
+        <div className="flex flex-wrap-reverse space-x-2">
+          {chosenTags.map((tag) => (
+            <Button
+              key={tag}
+              onClick={() => handleTagClick(tag)}
+              variant={"pub"}
+            >
+              {tag}
+            </Button>
+          ))}
         </div>
-        <Button type="submit" variant={"pub"}>Update suggestions</Button>
-      </form>
-    </Form>
-  )
+      </div>
+      <Separator className="w-max-prose my-4" />
+      <div className="mt-4">
+        <div className="flex flex-wrap space-x-2">
+          {unchosenTags.map((tag) => (
+            <Button
+              key={tag}
+              onClick={() => handleTagClick(tag)}
+              variant={"outline"}
+            >
+              {tag}
+            </Button>
+          ))}
+        </div>
+      </div>
+      <div>
+          <h3 className="mb-4 text-lg font-medium">Tags Suggestions</h3>
+          <div className="space-y-4"></div>
+        </div>
+        <Button type="submit" variant={"pub"}>
+          Update suggestions
+        </Button>
+    </div>
+
+    
+  );
 }
