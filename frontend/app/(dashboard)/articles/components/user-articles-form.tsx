0e503e8bff40761f4cf6328447953a7cc131c29e
Raymond Postrero <raymondpostrero1@gmail.com>
2023-12-06 08:18:53 +0800

    profile and user articles, including delete function

--- frontend/app/(dashboard)/articles/components/user-articles-form.tsx
+++ frontend/app/(dashboard)/articles/components/user-articles-form.tsx
@@ -1,6 +1,9 @@
 "use client";
 import Image from "next/image";
 import {FiBookmark} from 'react-icons/fi';
+import { FaEllipsisVertical } from "react-icons/fa6";
+import { FaTrashCan } from "react-icons/fa6";
+import { FaEdit } from "react-icons/fa";
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
@@ -23,14 +26,68 @@ interface Article {
     url: string;
 }
 
+interface PostCardProps extends Props {
+    isProfilePage: boolean; // Prop indicating if it's a profile page
+}
+
 interface Props {
     article: Article;
 }
+const PostCard = ({ article, isProfilePage }: PostCardProps) => {
+    const [isHovered, setIsHovered] = useState(false);
+    const [showDropdown, setShowDropdown] = useState(false); // State to manage dropdown visibility
 
-const PostCard = ({ article }: Props) => {
+    const handleHover = () => {
+        setIsHovered(true);
+    };
+
+    const handleUnhover = () => {
+        setIsHovered(false);
+        setShowDropdown(false);
+    };
+
+    
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+    const closeDropdown = () => {
+        setShowDropdown(false);
+    };
+
+    const deleteArticle = async () => {
+        try {
+            const token = localStorage.getItem("authToken");
+    
+            const response = await fetch(`http://localhost:8000${article.url}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+            });
+    
+            if (response.ok) {
+                console.log('Article deleted successfully');
+            } else {
+                const errorData = await response.json();
+                console.error('Failed to delete the article:', response.status, errorData);
+            }
+        } catch (error) {
+            console.error('Error deleting the article:', error);
+        }
+    };
 
     return (
-        <div className="flex gap-2 w-[700px]">
+        <div 
+            className={`flex gap-2 p-4 w-[700px] hover: ${isHovered ? 'transform scale-110' : ''}`}
+            onMouseEnter={handleHover}
+            onMouseLeave={handleUnhover}
+            style={{
+                boxShadow: isHovered ? '0px 4px 8px rgba(0, 0, 0, 0.1)' : 'none',
+                transition: 'transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out',
+            }}
+        >
             <div> 
                 <Link href={{ pathname:"/article-view", query:{viewurl: article.url} }} key={article.id}>
                     <h1 className="font-bold text-2xl cursor-pointer">{article.title}</h1>
@@ -55,9 +112,29 @@ const PostCard = ({ article }: Props) => {
                         <span className="text-sm text-gray-400">{article.date_created}</span>
                     </div>
 
-                    <span className="cursor-pointer ml-auto flex items-center px-2">
+                    <span className="cursor-pointer ml-auto flex items-center px-2 gap-2">
                         <FiBookmark className="h-5 w-5"/>
+                        {isProfilePage && (
+                            <FaEllipsisVertical className="h-5 w-5" onClick={toggleDropdown} />
+                        )}
+                        {showDropdown && isProfilePage && (
+                            <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
+                            {/* Dropdown content */}
+                            <div className="py-2 px-4">
+                                {/* Your dropdown items */}
+                                <span className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 roun">
+                                    <FaEdit />
+                                    <p className="">Edit post</p>
+                                </span>
+                                <span className="flex items-center gap-2 cursor-pointer hover:bg-gray-100" onClick={deleteArticle}>
+                                    <FaTrashCan />
+                                    <p className="">Delete article</p>
+                                </span>
+                            </div>
+                            </div>
+                        )}
                     </span>
+                    
                 </div>
 
                 <span className="bg-gray-300 p-1.5 rounded-full text-[.7rem]">
--- frontend/app/(user)/profile/[username]/page.tsx
+++ frontend/app/(user)/profile/[username]/page.tsx
@@ -3,19 +3,21 @@ import { useState, useEffect } from "react";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import axios from "axios";
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import PostCard from '@/app/(dashboard)/articles/components/user-articles-form';
 
-interface User {
-  username: string;
+interface UserData {
   name: string;
+  username: string;
   profile_picture: string;
   is_active: boolean;
-  followers: string[]; // Change the type as needed
-  following: string[]; // Change the type as needed
+  followers: [];
+  following: [];
   bio: string;
-  articles?: Article[]; // Assuming articles is an array of Article
+  article: ArticleData;
 }
-interface Article {
+interface ArticleData {
   id: number;
   title: string;
   content_preview: string;
@@ -34,47 +36,51 @@ interface Author {
 }
 
 const Profile: React.FC = () => {
-  const [user, setUser] = useState<User | null>(null);
-  const [articles, setArticles] = useState<Article[]>([]);
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [articleData, setArticleData] = useState<ArticleData[]>([]);
+  const router = useRouter();
 
   useEffect(() => {
-    const getUserData = async () => {
-      const authToken = localStorage.getItem("authToken");
-      try {
-        // Fetch user data
-        const response = await axios.get<User>(
-          "https://localhost:8000/settings/profile",
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${authToken}`,
-            },
-          }
-        );
+    const fetchData = async () => {
+      const token = localStorage.getItem('authToken');
+      if (token) {
+        try {
+          const userResponse = await axios.get<UserData>(
+            'http://localhost:8000/settings/profile/',
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
 
-        setUser(response.data);
+          setUserData(userResponse.data);
 
-        // Fetch user's articles
-        const articlesResponse = await axios.get<Article[]>(
-          `https://localhost:8000/api/${response.data.username}/articles/`,
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${authToken}`,
-            },
-          }
-        );
+          // Fetch user's articles data after fetching user data
+          const articlesResponse = await axios.get<ArticleData[]>(
+            `http://localhost:8000/api/${userResponse.data.username}/articles/`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
 
-        setArticles(articlesResponse.data);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
+          setArticleData(articlesResponse.data);
+          console.log("User Data:", userResponse.data);
+          console.log("Article Data:", articlesResponse.data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
       }
     };
 
-    getUserData();
-  }, []);
+    const intervalId = setInterval(fetchData, 1000); // Fetch data every 1 second
 
-  if (!user) {
+    return () => clearInterval(intervalId); // Clean up the interval on unmount
+  }, [router]);
+
+  if (!userData) {
     return <p>Loading...</p>;
   }
 
@@ -84,51 +90,45 @@ const Profile: React.FC = () => {
         <div className="flex items-center flex-col justify-center">
           <Avatar className="w-4/12 h-full">
             <AvatarImage
-              src={user.profile_picture}
-              alt={`@%${user.name}`}
+              src={userData.profile_picture}
+              alt={`@%${userData.name}`}
               className="rounded-full"
             />
-            <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
+            <AvatarFallback>{userData.name}</AvatarFallback>
           </Avatar>
           <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight mt-3">
-            {user.name}
+            {userData.name}
           </h2>
           <h3 className="scroll-m-20 text-2xl font-normal tracking-tight">
-            {user.username}
+            {userData.username}
           </h3>
         </div>
         <div className="flex flex-row items-center justify-center mx-auto space-x-4">
           <div className="flex-1">
-            <small>{user.followers.length} Followers</small>
+            <small>{userData.followers.length} Followers</small>
           </div>
           <div>
-            <small className="flex-1">{user.following.length} Following</small>
+            <small className="flex-1">{userData.following.length} Following</small>
           </div>
         </div>
         <div className="flex items-center justify-center">
-          <p className="leading-7 [&:not(:first-child)]:mt-6">{user.bio}</p>
+          <p className="leading-7 [&:not(:first-child)]:mt-6">{userData.bio}</p>
         </div>
       </div>
 
       <Separator className="w-max-prose mt-6" />
 
       <div className="max-w-2xl items-center space-x-2 justify-center space-y-2">
-        <div className="flex items-center justify-center space-y-2 my-2">
-          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-            Written Articles
+        <div className="flex items-center justify-center space-y-2 my-2 bg-slate-100 rounded-ee-full rounded-es-full">
+          <h3 className="scroll-m-20 text-4xl m-4 tracking-tight">
+            My Articles
           </h3>
         </div>
-        <div className="flex flex-row items-center justify-center mx-auto space-x-4">
-          {/* Map over user.articles and render each article */}
-          {user.articles &&
-            user.articles.map((article) => (
-              <div key={article.id}>
-                {/* Render the article details */}
-                <h4>{article.title}</h4>
-                <p>{article.content_preview}</p>
-                {/* Add other article details as needed */}
-              </div>
-            ))}
+        {/* </div><div className="flex flex-row items-center justify-center mx-auto space-x-4"> */}
+        <div className="flex flex-col gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols">
+          {articleData.map((article) => (
+            <PostCard key={article.id} article={article} isProfilePage={true} />
+          ))}
         </div>
       </div>
     </div>
