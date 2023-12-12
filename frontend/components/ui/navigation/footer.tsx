b46a6927c6d527453abf50691663ec3295d5f110
Kesha Torriana <mktorriana@tip.edu.ph>
2023-12-13 00:44:29 +0800

    fixed footer

--- frontend/components/ui/navigation/footer.tsx
+++ frontend/components/ui/navigation/footer.tsx
@@ -4,32 +4,18 @@ import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 export default function Footer() {
     return (
-        <footer style={{
-    return isVisible && (
-        <div style={{
-            bottom: '0',
-            width: '100%',
-            backgroundColor: 'white',
-            display: 'flex',
-            flexDirection: 'column', // Changed to column to stack items vertically
-            justifyContent: 'center', // Centered vertically
-            alignItems: 'center', // Centered horizontally
-            height: '100px', // Increased height to accommodate stacked items
-            boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.1)',
-            padding: '10px 20px', // Adjusted padding for spacing
-        }}>
-            <p style={{ margin: '0' }}>
-                © 2023 | All rights reserved | <span style={{ color: 'rgba(59, 178, 130, 0.54)', fontWeight: 'bold' }}>The Pub Official</span>
+        <div className="w-full bg-white flex flex-col justify-center items-center h-24 shadow-md shadow-gray-900 p-5">
+            <p className="m-0">
+                © 2023 | All Rights Reserved | <span className="text-green-300 font-bold">The Pub Official</span>
             </p>
-            <div  className="flex justify-center my-3">
-                <FaFacebookF size={30} style={{ margin: '0 10px' }} />
-                <FaTwitter size={30} style={{ margin: '0 10px' }} />
-                <FaInstagram size={30} style={{ margin: '0 10px' }} />
+            <div className="flex justify-center my-3">
+                <FaFacebookF size={30} className="mx-1"/>
+                <FaTwitter size={30} className="mx-1"/>
+                <FaInstagram size={30} className="mx-1"/>
             </div>
-        </footer>
+        </div>
     );
 }
-
 
 
 
