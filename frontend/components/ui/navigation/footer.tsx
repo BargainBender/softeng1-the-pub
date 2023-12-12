22f55f1b612b01fb4dc92543db73aaaf4e09383d
Kesha Torriana <mktorriana@tip.edu.ph>
2023-12-13 00:31:08 +0800

    footer fixed

--- frontend/components/ui/navigation/footer.tsx
+++ frontend/components/ui/navigation/footer.tsx
@@ -3,48 +3,33 @@ import React, { useState, useEffect } from 'react';
 import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 export default function Footer() {
-    // const [isVisible, setIsVisible] = useState(false);
-    // let lastScrollTop = 0;
-
-    // useEffect(() => {
-    //     window.onscroll = () => {
-    //         let st = window.pageYOffset || document.documentElement.scrollTop;
-    //         if (st > lastScrollTop) {
-    //             if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-    //                 setIsVisible(true);
-    //             }
-    //         } else {
-    //             setIsVisible(false);
-    //         }
-    //         lastScrollTop = st <= 0 ? 0 : st;
-    //     };
-    // }, []);
-
     return (
-        <footer style={{
-    return isVisible && (
-        <div style={{
-            bottom: '0',
-            width: '100%',
-            backgroundColor: 'white',
-            display: 'flex',
-            flexDirection: 'column', // Changed to column to stack items vertically
-            justifyContent: 'center', // Centered vertically
-            alignItems: 'center', // Centered horizontally
-            height: '100px', // Increased height to accommodate stacked items
-            boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.1)',
-            padding: '10px 20px', // Adjusted padding for spacing
-        }}>
-            <p style={{ margin: '0' }}>
-                © 2023 | All rights reserved | <span style={{ color: 'rgba(59, 178, 130, 0.54)', fontWeight: 'bold' }}>The Pub Official</span>
+        <div className="w-full bg-white flex flex-col justify-center items-center h-24  shadow-md shadow-gray-900 p-6">
+            <p className="m-0">
+                © 2023 | All rights reserved | <span className="text-green-300 font-bold">The Pub Official</span>
             </p>
-            <div  className="flex justify-center my-3">
-                <FaFacebookF size={30} style={{ margin: '0 10px' }} />
-                <FaTwitter size={30} style={{ margin: '0 10px' }} />
-                <FaInstagram size={30} style={{ margin: '0 10px' }} />
+            <div className="flex justify-center my-3">
+                <FaFacebookF size={30} className="mx-2" />
+                <FaTwitter size={30} className="mx-2" />
+                <FaInstagram size={30} className="mx-2" />
             </div>
         </div>
     );
 }
 
+//     <footer style={{
+    // return isVisible && (
+    //     <div style={{
+    //         bottom: '0',
+    //         width: '100%',
+    //         backgroundColor: 'white',
+    //         display: 'flex',
+    //         flexDirection: 'column', // Changed to column to stack items vertically
+    //         justifyContent: 'center', // Centered vertically
+    //         alignItems: 'center', // Centered horizontally
+    //         height: '100px', // Increased height to accommodate stacked items
+    //         boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.1)',
+    //         padding: '10px 20px', // Adjusted padding for spacing
+    //     }}>
+            // 
 
